45561dec574c2746d3db26cc9a4d27488275197f
tobyjohansen <83167511+tobyjohansen@users.noreply.github.com>
2021-09-09 15:56:29 +0200

    Added latest version of Bitbot Line Follower Robot

--- bitbotXL.py
+++ bitbotXL.py
@@ -1,29 +1,27 @@
 # Tobias Johansen - tobiaj@hiof.no
 from microbit import *
-import neopixel  # Neopixel library controls the NeoPixels lights on the bitbot
+import neopixel #Neopixel library controls the NeoPixels lights on the bitbot
 
-# Neopixel code is modified and taken from l33t.uk/bitbot
+#Neopixel code is modified and taken from l33t.uk/bitbot
 np = neopixel.NeoPixel(pin13, 12)
 
-
-# Methods that alter the lights on bitbot
+#Methods that alter the lights on bitbot
 def right_lights(Red, Green, Blue):
     for pixel_id in range(6, 12):
         np[pixel_id] = (Red, Green, Blue)
     np.show()
 
-
 def left_lights(Red, Green, Blue):
     for pixel_id in range(0, 6):
         np[pixel_id] = (Red, Green, Blue)
     np.show()
-
-
-# Line Sensor
+    
+    
+    
+#Line Sensor
 I2CADDR = 0x1c
 
-
-def get_line(bit):
+def getLine(bit):
     mask = 1 << bit
     value = 0
     try:
@@ -35,27 +33,25 @@ def get_line(bit):
     else:
         return 0
 
-
-# Motor functions
+#Motor functions
 def move_forward():
-    pin14.write_analog(200)
+    pin14.write_analog(250)
     pin12.write_analog(0)
-    pin16.write_analog(200)
+    pin16.write_analog(250)
     pin8.write_analog(0)
-
 
 def move_left():
-    pin14.write_analog(200)
+    pin14.write_analog(250)
     pin12.write_analog(0)
-    sleep(400)
-
+    pin16.write_analog(0)
+    pin8.write_analog(0)
 
 def move_right():
-    pin16.write_analog(200)
+    pin16.write_analog(250)
     pin8.write_analog(0)
-    sleep(400)
-
-
+    pin14.write_analog(0)
+    pin12.write_analog(0)
+    
 def stop():
     pin14.write_analog(0)
     pin12.write_analog(0)
@@ -64,26 +60,18 @@ def stop():
 
 
 while True:
-    left_line = get_line(0)
-    right_line = get_line(1)
-    if left_line == 1:
-        stop()
+    left_line = getLine(0)
+    right_line = getLine(1)
+    if(left_line == 1):
+        left_lights(200,0,0)
         move_left()
-    elif right_line == 1:
-        stop()
+    elif(right_line == 1):
+        right_lights(200,0,0)
         move_right()
-    elif (left_line == 1) and (right_line == 1):
-        stop()
-    else:
-        stop()
+    elif(left_line == 0) and (right_line == 0):
+        right_lights(0,200,0)
+        left_lights(0,200,0)
         move_forward()
-
-while True:
-    if left_line == 1:
-        left_lights(0, 200, 0)
     else:
-        left_lights(200, 0, 0)
-    if right_line == 1:
-        right_lights(0, 200, 0)
-    else:
-        right_lights(200, 0, 0)
+        left_lights(0,200,0)
+        right_lights(0,200,0)
